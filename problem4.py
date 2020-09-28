869ce2ae07e3850f81ae274372853b80e3a0d1e6
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-09-28 13:04:59 -0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -12,3 +12,18 @@
 # - "that is a right triangle"
 # - "that is an acute triangle"
 # - "that is an obtuse triangle"
+
+import math
+
+a = input ("Enter one side of a triangle")
+b = input("Enter another side of the triangle")
+c = input("Enter the third side of the triangle")
+if a > b:
+    if c >= a:
+        hypotenuse = math.sqrt(float(a)**2 + float(b)**2)
+        if float(hypotenuse) - float(c) <= 0.02*float(c):
+            print("that is a right triangle")
+        elif float(hypotenuse) - float(c) < 0:
+            print("that is an obtuse triangle")
+        else:
+            print("that is an acute triangle")
