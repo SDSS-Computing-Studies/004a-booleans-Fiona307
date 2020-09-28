ef22d2d91f0ab5719cb60edfa5af9dda7d2cc27b
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-09-28 11:35:07 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -11,3 +11,15 @@
 # "The number is larger than 100"
 # "The number is smaller than 100"
 # "The number is 100"
+
+number = input("Enter a number")
+larger = float(number) > 100
+smaller = float(number) < 100
+equal = float(number) == 100
+
+if larger:
+    print("The number is larger than 100")
+if smaller:
+    print("The number is smaller than 100")
+if equal:
+    print("The number is 100")
